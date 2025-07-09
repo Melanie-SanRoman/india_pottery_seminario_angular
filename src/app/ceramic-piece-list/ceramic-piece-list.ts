f1f7e949940719ca102d83d8d99bff1f2f76be50
Melanie San Roman <srmel123@gmail.com>
2025-07-09 14:23:01 -0300

    Refactor CeramicPieceList to use an array of CeramicPiece objects for better data management

--- src/app/ceramic-piece-list/ceramic-piece-list.ts
+++ src/app/ceramic-piece-list/ceramic-piece-list.ts
@@ -1,18 +1,46 @@
 import { Component } from '@angular/core';
+import { CeramicPiece } from './CeramicPiece';
 
 @Component({
   selector: 'app-ceramic-piece-list',
   standalone: false,
   templateUrl: './ceramic-piece-list.html',
-  styleUrl: './ceramic-piece-list.scss'
+  styleUrl: './ceramic-piece-list.scss',
 })
 export class CeramicPieceList {
-  piece = {
-    "name": "Cafetino",
-    "line": "Rose",
-    "price": 25000,
-    "stock": 5,
-    "image" : "../assets/images/cafetino_rose.jpg"
-  }
-  constructor(){}
+  pieces: CeramicPiece[] = [
+    {
+      name: 'Cafetino',
+      line: 'Rose',
+      price: 12000,
+      stock: 5,
+      image: '../assets/images/cafetino_rose.jpg',
+      clearence: false
+    },
+    {
+      name: 'Jarra',
+      line: 'Rose',
+      price: 25000,
+      stock: 0,
+      image: '../assets/images/jarra_rose.jpg',
+      clearence: false
+    },
+    {
+      name: 'Cuenco',
+      line: 'Love',
+      price: 17000,
+      stock: 10,
+      image: '../assets/images/jarra_rose.jpg',
+      clearence: true
+    },
+    {
+      name: 'Budinera',
+      line: 'Gaspeada',
+      price: 22000,
+      stock: 2,
+      image: '../assets/images/jarra_rose.jpg',
+      clearence: false
+    },
+  ];
+  constructor() {}
 }
