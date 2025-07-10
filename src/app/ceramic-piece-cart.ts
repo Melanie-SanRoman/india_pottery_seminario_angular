1089a89aca622cea2ee6c6b40bdebe5457105df4
Melanie San Roman <srmel123@gmail.com>
2025-07-10 07:06:07 -0300

    Create the addToCart function in the TypeScript file of the ceramic-piece-cart service.

--- src/app/ceramic-piece-cart.ts
+++ src/app/ceramic-piece-cart.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { CeramicPiece } from './ceramic-piece-list/CeramicPiece';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CeramicPieceCart {
 
+  private _cartList: CeramicPiece[] = [];
+  cartList: BehaviorSubject<CeramicPiece[]> = new BehaviorSubject(this._cartList);
+
   constructor() { }
+
+  addToCart(piece: CeramicPiece) {
+    const existingPiece = this._cartList.find((item) => item.name === piece.name);
+    if (existingPiece) {
+      existingPiece.quantity += piece.quantity;
+    } else {
+      this._cartList.push({...piece});
+    }
+  }
 }
