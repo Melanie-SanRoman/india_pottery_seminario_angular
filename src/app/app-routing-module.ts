44458212365bbb09c7ff20918c98a83719615e91
Melanie San Roman <srmel123@gmail.com>
2025-07-10 02:26:17 -0300

    Add routing configuration for IndiaPottery components

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndiaPotteryPieces } from './india-pottery-pieces/india-pottery-pieces';
+import { IndiaPotteryAbout } from './india-pottery-about/india-pottery-about';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'pieces',
+    pathMatch: 'full'
+  },
+  {
+    path: 'pieces',
+    component: IndiaPotteryPieces
+  },
+  {
+    path: 'about',
+    component: IndiaPotteryAbout
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
