76f2426144493f732ad8f7160c548b0c499daf58
Melanie San Roman <srmel123@gmail.com>
2025-07-10 05:00:05 -0300

    Remove functions that were refactored in 'input-integer.ts'.

--- src/app/ceramic-piece-list/ceramic-piece-list.ts
+++ src/app/ceramic-piece-list/ceramic-piece-list.ts
@@ -16,7 +16,6 @@ export class CeramicPieceList {
       stock: 5,
       image: '../assets/images/cafetino_rose.jpg',
       clearance: false,
-      description: 'Descripcion...',
       quantity: 0,
     },
     {
@@ -26,7 +25,6 @@ export class CeramicPieceList {
       stock: 0,
       image: '../assets/images/jarra.jpg',
       clearance: false,
-      description: 'Descripcion...',
       quantity: 0,
     },
     {
@@ -36,7 +34,6 @@ export class CeramicPieceList {
       stock: 10,
       image: '../assets/images/cuenco.jpg',
       clearance: true,
-      description: 'Descripcion...',
       quantity: 0,
     },
     {
@@ -46,36 +43,8 @@ export class CeramicPieceList {
       stock: 2,
       image: '../assets/images/budinera.jpg',
       clearance: false,
-      description: 'Descripcion...',
       quantity: 0,
     },
   ];
   constructor() {}
-
-  upQuantity(piece: CeramicPiece): void {
-    if (piece.stock > 0) {
-      piece.quantity++;
-      piece.stock--;
-    }
-  }
-
-  downQuantity(piece: CeramicPiece): void {
-    if (piece.quantity > 0) {
-      piece.quantity--;
-      piece.stock++;
-    }
-  }
-
-  changeQuantity($event: Event, piece: CeramicPiece) {
-    const input = $event.target as HTMLInputElement;
-    const value = parseInt(input.value);
-    const maxQuantity = piece.quantity + piece.stock;
-    if (!isNaN(value) && value >= 0 && value <= maxQuantity) {
-      piece.stock = maxQuantity - value;
-      piece.quantity = value;
-    } else {
-      // Si el valor no es válido, restaurar el valor anterior
-      input.value = piece.quantity.toString();
-    }
-  }
 }
