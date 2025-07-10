7dca7294b0a61ab6636d16dec9953a20d1ea51b7
Melanie San Roman <srmel123@gmail.com>
2025-07-09 23:45:50 -0300

    Add quantity management methods and initialize quantity for ceramic pieces

--- src/app/ceramic-piece-list/ceramic-piece-list.ts
+++ src/app/ceramic-piece-list/ceramic-piece-list.ts
@@ -17,6 +17,7 @@ export class CeramicPieceList {
       image: '../assets/images/cafetino_rose.jpg',
       clearance: false,
       description: 'Descripcion...',
+      quantity: 0,
     },
     {
       name: 'Jarra',
@@ -26,6 +27,7 @@ export class CeramicPieceList {
       image: '../assets/images/jarra.jpg',
       clearance: false,
       description: 'Descripcion...',
+      quantity: 0,
     },
     {
       name: 'Cuenco',
@@ -35,6 +37,7 @@ export class CeramicPieceList {
       image: '../assets/images/cuenco.jpg',
       clearance: true,
       description: 'Descripcion...',
+      quantity: 0,
     },
     {
       name: 'Budinera',
@@ -44,7 +47,35 @@ export class CeramicPieceList {
       image: '../assets/images/budinera.jpg',
       clearance: false,
       description: 'Descripcion...',
+      quantity: 0,
     },
   ];
   constructor() {}
+
+  upQuantity(piece: CeramicPiece): void {
+    if (piece.stock > 0) {
+      piece.quantity++;
+      piece.stock--;
+    }
+  }
+
+  downQuantity(piece: CeramicPiece): void {
+    if (piece.quantity > 0) {
+      piece.quantity--;
+      piece.stock++;
+    }
+  }
+
+  changeQuantity($event: Event, piece: CeramicPiece) {
+    const input = $event.target as HTMLInputElement;
+    const value = parseInt(input.value);
+    const maxQuantity = piece.quantity + piece.stock;
+    if (!isNaN(value) && value >= 0 && value <= maxQuantity) {
+      piece.stock = maxQuantity - value;
+      piece.quantity = value;
+    } else {
+      // Si el valor no es válido, restaurar el valor anterior
+      input.value = piece.quantity.toString();
+    }
+  }
 }
