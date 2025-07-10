2cac039a1b0a1621d8daa2e90b3abc8525c1cd78
Melanie San Roman <srmel123@gmail.com>
2025-07-10 00:58:01 -0300

    Add IndiaPotteryAbout component with HTML and SCSS files

---
+++ src/app/india-pottery-about/india-pottery-about.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-india-pottery-about',
+  standalone: false,
+  templateUrl: './india-pottery-about.html',
+  styleUrl: './india-pottery-about.scss'
+})
+export class IndiaPotteryAbout {
+
+}
