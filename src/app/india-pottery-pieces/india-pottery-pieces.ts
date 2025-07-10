669483707270725b62c46798bc57a41557c8c2d0
Melanie San Roman <srmel123@gmail.com>
2025-07-10 00:58:55 -0300

    Add IndiaPotteryPieces component with HTML, SCSS, and TypeScript files

---
+++ src/app/india-pottery-pieces/india-pottery-pieces.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-india-pottery-pieces',
+  standalone: false,
+  templateUrl: './india-pottery-pieces.html',
+  styleUrl: './india-pottery-pieces.scss'
+})
+export class IndiaPotteryPieces {
+
+}
