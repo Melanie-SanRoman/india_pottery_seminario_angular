3edef50bf1de921b6b0a00a892270a6b67585a46
Melanie San Roman <srmel123@gmail.com>
2025-07-15 18:16:45 -0300

    Declaring a reactive form in the IndiaPotteryContact component and defining the onSubmit() function.

--- src/app/india-pottery-contact/india-pottery-contact.ts
+++ src/app/india-pottery-contact/india-pottery-contact.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-india-pottery-contact',
@@ -8,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class IndiaPotteryContact {
 
+  successMessage = false;
+
+  contactForm = new FormGroup({
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    message: new FormControl('', [Validators.required]),
+  });
+
+  onSubmit() {
+  if (this.contactForm.valid) {
+    console.log('Form Submitted!', this.contactForm.value);
+
+    this.successMessage = true; // Muestra el mensaje
+    this.contactForm.reset();   // Limpia el formulario
+
+    // Oculta el mensaje luego de 5 segundos
+    setTimeout(() => {
+      this.successMessage = false;
+    }, 5000);
+  }
+}
 }
