0a27eeac59cdfaaa50895e8774d191cd226f58ea
Melanie San Roman <srmel123@gmail.com>
2025-07-15 04:54:27 -0300

    Declare the function that calls the Rest API.

--- src/app/ceramic-data.ts
+++ src/app/ceramic-data.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap, map, Observable } from 'rxjs';
+import { CeramicPiece } from './ceramic-piece-list/CeramicPiece';
+
+const URL = 'https://6875f57f814c0dfa653a01da.mockapi.io/india_pottery/CeramicPiece';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CeramicData {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getAll(): Observable<CeramicPiece[]> {
+
+    return this.http.get<CeramicPiece[]>(URL).pipe(
+      tap((pieces: CeramicPiece[]) => pieces.forEach(piece => piece.quantity = 0))
+    );
+  }
 }
