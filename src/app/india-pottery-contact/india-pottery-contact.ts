06d216c73ec2e1b2ae58aafca2a63fa9bf13ca94
Melanie San Roman <srmel123@gmail.com>
2025-07-15 06:22:50 -0300

    Add IndiaPotteryContact component with HTML, SCSS, and TypeScript files

---
+++ src/app/india-pottery-contact/india-pottery-contact.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-india-pottery-contact',
+  standalone: false,
+  templateUrl: './india-pottery-contact.html',
+  styleUrl: './india-pottery-contact.scss'
+})
+export class IndiaPotteryContact {
+
+}
