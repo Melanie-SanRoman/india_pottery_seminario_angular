18b604f09bacd29859d55ca2c589b37b4c3e6008
Melanie San Roman <srmel123@gmail.com>
2025-07-17 23:36:36 -0300

    Modify the template and design logic in the ShoppingCart component.

--- src/app/shopping-cart/shopping-cart.ts
+++ src/app/shopping-cart/shopping-cart.ts
@@ -1,19 +1,42 @@
 import { Component } from '@angular/core';
 import { CeramicPieceCart } from '../ceramic-piece-cart';
 import { CeramicPiece } from '../ceramic-piece-list/CeramicPiece';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-cart',
   standalone: false,
   templateUrl: './shopping-cart.html',
-  styleUrl: './shopping-cart.scss'
+  styleUrl: './shopping-cart.scss',
 })
 export class ShoppingCart {
-
   cartList$: Observable<CeramicPiece[]>;
+  total$: Observable<number>;
+  // purchaseCompleted = false;
 
   constructor(private cart: CeramicPieceCart) {
     this.cartList$ = cart.cartList.asObservable();
+    this.total$ = this.cartList$.pipe(
+      map((items) =>
+        items.reduce((acc, item) => acc + item.price * item.quantity, 0)
+      )
+    );
   }
+
+  removeFromCart(piece: CeramicPiece) {
+    const currentCart = this.cart.cartList.value; // Obtener valor actual
+    const pieceInCart = currentCart.find((p) => p.id === piece.id);
+  }
+
+  // finalizePurchase() {
+  //   this.purchaseCompleted = true;
+
+  //   // Acá podrías limpiar el carrito también:
+  //   this.cart.cartList.next([]);
+
+  //   // Ocultar el mensaje luego de unos segundos
+  //   setTimeout(() => {
+  //     this.purchaseCompleted = false;
+  //   }, 4000);
+  // }
 }
