80494952ebfaf72c2cd5a5560d5ffb71279d5f48
Melanie San Roman <srmel123@gmail.com>
2025-07-17 23:52:37 -0300

    Add IndiaPotteryReviews component with HTML, SCSS, and TypeScript files.

---
+++ src/app/india-pottery-reviews/india-pottery-reviews.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-india-pottery-reviews',
+  standalone: false,
+  templateUrl: './india-pottery-reviews.html',
+  styleUrl: './india-pottery-reviews.scss'
+})
+export class IndiaPotteryReviews {
+
+}
